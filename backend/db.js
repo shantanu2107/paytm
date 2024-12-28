9b6535700d3b7531b4c6c062a541ea3b004abae2
Shantanu Chauhan <shantanuchauhan@Shantanus-MacBook-Air.local>
2024-12-29 01:58:31 +0530

    initial commit

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 // backend/db.js
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/paytm")
+mongoose.connect("mongodb+srv://shaan2107:Rekhashaan%402001@cluster0.xa27a.mongodb.net/shaan")
 
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
